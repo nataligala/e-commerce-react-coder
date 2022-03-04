aa3466c6f4dfde7bc8d56702527a038c47133173
nataligala <natali.rabuffetti@gmail.com>
2022-03-04 02:42:11 -0300

    Mod color, foto, fuente sobre HomeView - borrado de elementos no necesarios en NavBar

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,16 +1,24 @@
-import { Container } from "@mui/material"
+// import { Container } from "@mui/material"
+import "./HomeView.css";
+import productos from "../../assets/img_homeview/productos.png"
+import { Container } from "@mui/material";
 
 export const HomeView = () => {
     return (
         
-        <Container>
+        <>
             <section className="homeView">
                 <div className="homeView__container">
-                    <h2>Productos</h2>
-                    <h4>¿Qué tenés ganas de comer hoy?</h4>
-                    <p>Conocé todas nuestras opciones ricas y saludables</p>
+                    <h2 className="homeView__title">Productos</h2>
+                    <h3 className="homeView__subtitle">¿Qué tenés ganas de comer hoy?</h3>
+                    <p className="homeView__description">Conocé todas nuestras opciones ricas y saludables</p>
                 </div>
             </section>
-        </Container>
+            
+            <Container>
+                <img alt="Productos" src={productos} className="homeView__photo"></img>
+            </Container>
+
+        </>
     )
 }        
