9c91f86482379d166d1e7e902c84059657257cbc
nataligala <natali.rabuffetti@gmail.com>
2022-03-04 01:10:02 -0300

    Clogo en NavBar - Creacion HomeView

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material"
+
+export const HomeView = () => {
+    return (
+        
+        <Container>
+            <section className="homeView">
+                <div className="homeView__container">
+                    <h2>Productos</h2>
+                    <h4>¿Qué tenés ganas de comer hoy?</h4>
+                    <p>Conocé todas nuestras opciones ricas y saludables</p>
+                </div>
+            </section>
+        </Container>
+    )
+}        
